3df1621542283cfb6f71fe36ff1a40d552cd1d70
박건규 <geongyu09@gmail.com>
2026-08-05 16:07:57 +0900

    chore: typescript 설정 파일 추가
    #1

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js", // 모듈 진입점
+  entry: "./src/index.ts", // 모듈 진입점
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
@@ -17,5 +17,17 @@ module.exports = {
     client: {
       overlay: true, // 빌드 오류 시 브라우저에 오버레이 표시
     },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/, // .ts 파일들은
+        use: "ts-loader", // ts-loader를 거쳐 처리돼요.
+        exclude: /node_modules/, // 외부 모듈은 제외해요.
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
   },
 };
