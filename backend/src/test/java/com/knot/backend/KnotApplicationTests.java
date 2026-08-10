3fd936312c9712eeddb8cdaad8d7d7edd9a9c496
jyt6640 <jyt6640@gmail.com>
2026-08-10 11:13:46 +0900

    chore: 백엔드 프로젝트 환경 셋팅

---
+++ backend/src/test/java/com/knot/backend/KnotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knot.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class KnotApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
---
+++ backend/src/test/java/com/knot/backend/TestBackendApplication.java
@@ -0,0 +1,12 @@
+package com.knot.backend;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(KnotApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+    }
+}
