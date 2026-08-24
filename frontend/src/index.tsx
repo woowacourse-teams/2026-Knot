d59e938fb0dfa1e617b8f7e81afd984255b240fd
김예안 <kjan0929@naver.com>
2026-08-24 20:07:55 +0900

    chore: queryClientProvider 세팅
    index.tsx 불필요 console.log 제거
    
    #174

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./shared/provider/queryClient";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 );
-
-console.log("Hello, World!");
