80dcc3bf6ee3e46f5ccbdb09bb87cf6795ccd56d
김예안 <kjan0929@naver.com>
2026-08-25 14:14:11 +0900

    refactor: axios timeout 리터럴 상수화
    #180

--- frontend/src/shared/api/httpClient/index.ts
+++ frontend/src/shared/api/httpClient/index.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const TIMEOUT_DURATION = 10_000;
+
 export const httpClient = axios.create({
   baseURL: process.env.API_BASE_URL,
-  timeout: 10_000,
+  timeout: TIMEOUT_DURATION,
 });
