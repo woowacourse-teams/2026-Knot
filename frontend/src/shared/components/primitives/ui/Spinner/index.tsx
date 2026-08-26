375a820b432a64bf5116bcf9690d993f03fff132
eastar-DS <dororo0104@naver.com>
2026-08-26 20:03:45 +0900

    chore: Spinner 코드 프리티어 적용

--- frontend/src/shared/components/primitives/ui/Spinner/index.tsx
+++ frontend/src/shared/components/primitives/ui/Spinner/index.tsx
@@ -4,30 +4,30 @@ import type { HTMLAttributes } from "react";
 
 import spinnerMask from "@/assets/spinnerMask.svg";
 
-interface SpinnerProps extends HTMLAttributes<HTMLSpanElement>{
-    size?: string;
+interface SpinnerProps extends HTMLAttributes<HTMLSpanElement> {
+  size?: string;
 }
 
-export default function Spinner({size = "1.5rem", ...props}: SpinnerProps) {
-    return <Wrapper $size={size} aria-hidden {...props} />;
+export default function Spinner({ size = "1.5rem", ...props }: SpinnerProps) {
+  return <Wrapper $size={size} aria-hidden {...props} />;
 }
 
 const spin = keyframes`
     to {
         transform: rotate(1turn);
     }
-`
+`;
 
-const Wrapper = styled.span<{$size: string}>`
-    display: block;
-    flex-shrink: 0;
-    width: ${({$size}) => $size};
-    height: ${({$size}) => $size};
-    
-    background: conic-gradient(from 90deg, currentColor, transparent);
+const Wrapper = styled.span<{ $size: string }>`
+  display: block;
+  flex-shrink: 0;
+  width: ${({ $size }) => $size};
+  height: ${({ $size }) => $size};
 
-    -webkit-mask: url("${spinnerMask}") center / contain no-repeat;
-    mask: url("${spinnerMask}") center / contain no-repeat;
+  background: conic-gradient(from 90deg, currentColor, transparent);
 
-    animation: ${spin} 0.6s linear infinite;
+  -webkit-mask: url("${spinnerMask}") center / contain no-repeat;
+  mask: url("${spinnerMask}") center / contain no-repeat;
+
+  animation: ${spin} 0.6s linear infinite;
 `;
