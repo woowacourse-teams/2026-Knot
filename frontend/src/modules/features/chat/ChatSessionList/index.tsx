d93cc4bddcda1a3cf8b2044a62119e7c1bcff5bd
김예안 <kjan0929@naver.com>
2026-08-31 17:18:39 +0900

    feat: 채팅 패널 헤더, 빈 화면 안내 UI 구현
    #234

---
+++ frontend/src/modules/features/chat/ChatSessionList/index.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+/**
+ * 워크스페이스에 쌓인 대화 목록.
+ *
+ * @see https://www.figma.com/design/jyDFCKX5AIztZessq4H7nQ/knot?node-id=600-10217
+ */
+// TODO: 대화 목록 UI 구현
+export default function ChatSessionList() {
+  return <Container>ChatSessionList</Container>;
+}
+
+const Container = styled.div``;
