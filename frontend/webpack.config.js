4172bba6f13c83dd86144d659308772dee7a6d37
박건규 <geongyu09@gmail.com>
2026-08-31 15:43:27 +0900

    fix: 프로덕션 번들에서 jsxDEV 호출로 렌더링이 실패하던 문제 수정
    @babel/preset-react 8은 development 옵션이 없으면 Babel 환경
    (NODE_ENV, 기본값 development)을 따르는데, webpack --mode production은
    Node 프로세스의 NODE_ENV를 바꾸지 않아 jsxDEV 호출이 생성되었습니다.
    번들 내부는 production으로 치환되어 react/jsx-dev-runtime의 jsxDEV가
    undefined가 되므로 development를 isDev로 명시합니다.
    
    #161

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -52,6 +52,7 @@ export default (env, argv) => {
                     {
                       runtime: "automatic",
                       importSource: "@emotion/react", // css prop을 위해 emotion의 jsx로 변환해요
+                      development: isDev, // 프로덕션 빌드에서는 jsxDEV 대신 jsx를 사용해요
                     },
                   ],
                   "@babel/preset-typescript", // 타입스크립트를 변환해요
