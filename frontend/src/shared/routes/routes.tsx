e6d93ac1c81c14ff923a47c419c4f7a61de6fe68
박건규 <geongyu09@gmail.com>
2026-08-31 22:18:44 +0900

    feat: 초대 링크 진입 페이지를 /invite/:token 라우트에 연결
    #264

--- frontend/src/shared/routes/routes.tsx
+++ frontend/src/shared/routes/routes.tsx
@@ -1,6 +1,7 @@
 import CenteredLayout from "@pages/_layout/CenteredLayout";
 import ChatLayout from "@pages/_layout/ChatLayout";
 import WorkspaceLayout from "@pages/_layout/WorkspaceLayout";
+import InvitePage from "@pages/invite/[token]";
 import JoinErrorPage from "@pages/join-error";
 import LoginPage from "@pages/login";
 import OnboardingPage from "@pages/onboarding";
@@ -54,6 +55,10 @@ export const router = createBrowserRouter([
       {
         path: PATH_ROUTE.WORKSPACE_CODE,
         element: <WorkspaceCodePage />,
+      },
+      {
+        path: PATH_ROUTE.INVITE,
+        element: <InvitePage />,
       },
       {
         path: PATH_ROUTE.WORKSPACE_JOIN,
